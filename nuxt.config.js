a611410cfcfa3543720d09ccf81eaf81ae8ce5d7
puterakahfi <puterakahfi@gmail.com>
2019-03-29 07:41:24 +0700

    add stisla

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,13 @@ const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: "/<repository-name>/"
+          base: "/nuxt-cv/"
         }
       }
     : {};
 
 export default {
   mode: "universal",
-
-  router: {
-    base: "/nuxt-cv/"
-  },
 
   /*
    ** Headers of the page
@@ -27,6 +23,7 @@ export default {
       { hid: "description", name: "description", content: pkg.description }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    
   },
 
   /*
