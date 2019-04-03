c048dfa86470204335bd76a7dbe27e5150d377f0
puterakahfi <puterakahfi@gmail.com>
2019-04-03 11:46:10 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row align-items-center" id="head">
         <div class="col-md-6 col-sm-12">
-          <h1>Putera Kahfi</h1>
+          <h1 class="display-3">Putera Kahfi</h1>
           <p class="lead">Web Developer.</p>
           <div class="cta">
             <a
@@ -40,24 +40,5 @@ export default {
 </script>
 
 <style sccoped>
-#head h1 {
-  font-weight: bolder;
-  font-size: 5rem;
-  color:#000000;
-}
 
-#head .lead {
-  font-size: 3rem;
-  margin-bottom: 60px;
-
-  color: #000000;
-}
-
-.hero-mini {
-  background-image: #0f0f0f;
-  padding-top: 40px;
-  height: 500px;
-  display: flex;
-  align-items: center;
-}
 </style>
