12f491326b508ac66c1f134dd9c6724523598e44
puterakahfi <puterakahfi@gmail.com>
2019-04-17 13:23:28 +0700

    update navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <nuxt-link to="/" class="navbar-brand smooth h1">Putera Kahfi</nuxt-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto ml-lg-3 align-items-lg-center">
+        <li class="nav-item">
+          <a class="nav-link" target="_blank" href="https://medium.com/@puterakahfi/">Blog</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto align-items-lg-center d-none d-lg-block">
+        <li class="ml-lg-3 nav-item">
+          <a
+            href="https://github.com/puterakahfi"
+            class="btn btn-dark smooth btn-icon icon-left shadowless"
+            target="_blank"
+          >
+            <fa :icon="['fab', 'github']"/> Github
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+
+<style scoped>
+.navbar-light .navbar-nav .nav-link {
+  color: rgba(0, 0, 0, 0.5);
+  font-weight: bold;
+}
+
+.navbar .navbar-brand {
+  color: #6c757d;
+  text-transform: inherit;
+  font-weight: 700;
+}
+
+.navbar {
+  left: 0;
+  right: 0;
+  background: white;
+  height: auto;
+  padding: 12px 0px;
+}
+</style>
