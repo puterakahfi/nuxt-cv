64b058d9827c1f8f30cba42a78d6d3b054d084dd
puterakahfi <puterakahfi@gmail.com>
2019-06-29 13:59:01 +0700

    fix issue when generate ,zzzzz

--- components/SkillsetCard.vue
+++ components/SkillsetCard.vue
@@ -2,12 +2,14 @@
 <template>
   <div>
     <div class="columns is-multiline">
-      <div class="column is-6" v-for="skill in data.items" :key="skill.label">
-        <div class="box">
+      <div class="column is-12" v-for="skill in data.items" :key="skill.label">
+        <div class="box has-margin-top-5 is-paddingless">
           <article class="media">
             <div class="media-left">
               <figure class="image is-64x64">
-                <img :src="skill.image" alt="Image">
+                <a :href="skill.url" target="_blank">
+                  <img :src="skill.image" alt="Image">
+                </a>
               </figure>
             </div>
             <div class="media-content">
@@ -24,9 +26,126 @@
           </article>
         </div>
       </div>
+
+      <nav class="level is-mobile other-skill">
+        <div class="level-item has-text-centered has-margin-left-10">
+          <div>
+            <p class="heading"></p>
+            <p class="subtitle">Other:</p>
+          </div>
+        </div>
+
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip label="vuejs:beginner" type="is-dark" position="is-bottom" class="title">
+                <fa :icon="['fab', 'vuejs']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip
+                label="docker:intermediate"
+                type="is-dark"
+                position="is-bottom"
+                class="title"
+              >
+                <fa :icon="['fab', 'docker']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip label="git:intermediate" type="is-dark" position="is-bottom" class="title">
+                <fa :icon="['fab', 'git']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip
+                label="bootstrap:intermediate"
+                type="is-dark"
+                position="is-bottom"
+                class="title"
+              >
+                <fa :icon="['fab', 'bootstrap']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip label="ubuntu:enduser" type="is-dark" position="is-bottom" class="title">
+                <fa :icon="['fab', 'ubuntu']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip label="sass:beginner" type="is-dark" position="is-bottom" class="title">
+                <fa :icon="['fab', 'sass']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading"></p>
+            <p class="title">
+              <b-tooltip
+                label="markdown:beginner"
+                type="is-dark"
+                position="is-bottom"
+                class="title"
+              >
+                <fa :icon="['fab', 'markdown']"/>
+              </b-tooltip>
+            </p>
+          </div>
+        </div>
+      </nav>
     </div>
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+.box {
+  padding: 1em;
+  box-shadow: none;
+  border-radius: none;
+}
+
+.other-skill {
+  .title {
+    font-size: 1.3rem;
+    color: #bebece;
+  }
+
+  .subtitle {
+    font-size: 1rem;
+    color: #bebece;
+  }
+}
+</style>
 
 
 
