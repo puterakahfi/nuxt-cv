bb522494146cb50f9cc09a76a5c8656600f1b08b
puterakahfi <puterakahfi@gmail.com>
2019-09-04 19:24:31 +0700

    66436b3

---
+++ _nuxt/a828143a8bfe4d2ee252.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{333:function(n,t,e){"use strict";e.r(t);var c={},l=e(22),component=Object(l.a)(c,function(){var n=this.$createElement;return(this._self._c||n)("section",[this._v("SOLID TEMPLATE HERE")])},[],!1,null,null,null);t.default=component.exports}}]);
